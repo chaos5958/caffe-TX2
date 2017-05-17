efbcda2a16a1ae6f94a4a8aab6c295db5513be89
Hyunho Yeo <chaos5958@gmail.com>
2017-05-18 04:02:16 +0900

    Json pase done

--- tracker/client.py
+++ tracker/client.py
@@ -15,24 +15,51 @@ FLAG = True
 
 def recv_msg():
     while True:
-        time.sleep(1)
-        data = conn.recv(1024)
-        input = json.loads(data)
-        print ' '
-        print "recv msg", repr(data)
-        #pprint(input)
-        #print "status", input['cmd'], "data", input['target']['index']
+        raw_data = conn.recv(4096)
+        #print repr(raw_data)
+        json_list = []
+        json_list = raw_data.split("}\n{");
+        for i in range(len(json_list)):
+            print i, len(json_list)
+            if i == len(json_list) - 1:
+                if len(json_list) == 1:
+                    parse_json(json_list[0])
+                else:
+                    json_list[i] = "{" + json_list[i]
+                    data = json_list[i]
+                    parse_json(data)
+            else:
+                if i == 0:
+                    json_list[i] = json_list[i] + "}"
+                    data = json_list[i]
+                    parse_json(data)
+                if i != 0:
+                    json_list[i] = "{" + json_list[i] + "}"
+                    data = json_list[i]
+                    parse_json(data)
 
-        if input['status'] is "MULTI_OBJECTS":
-            print "MULTI"
-        elif input['status'] is "NO_OBJECTS":
-            print "NO_OBJ"
-        elif input['status'] is "SUCCESS":
-            x_min  = input['data'][0]
-            y_min  = input['data'][1]
-            width  = input['data'][2]
-            height = input['data'][3]
-            print x_min, y_min, width, height
+
+
+
+def parse_json(data):
+    #print repr(data)
+    input = json.loads(data)
+    print ' '
+    #print "recv msg", repr(data)
+    #pprint(input)
+    #print "status", input['cmd'], "data", input['target']['index']
+
+    if input['status'].startswith("MULTI_OBJECTS"):
+        print "MULTI"
+    elif input['status'].startswith("NO_OBJECTS"):
+        print "NO_OBJ"
+    elif input['status'].startswith("SUCCESS"):
+        x_min  = input['data']['x_min']
+        y_min  = input['data']['y_min']
+        width  = input['data']['width']
+        height = input['data']['height']
+        print x_min, y_min, width, height
+
 
 conn = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 conn.connect((HOST_IP, HOST_PORT))
