f81869418b2f37c5a27ec4462b037bbcd7c3d15c
githjh <thundershower@kaist.ac.kr>
2017-05-18 02:09:26 +0900

    client.py update for debugging

--- tracker/client.py
+++ tracker/client.py
@@ -16,31 +16,45 @@ conn.connect((HOST_IP, HOST_PORT))
 try:
     # threading._start_new_thread(sending_msg, ())
     while FLAG:
-        time.sleep(1)
+        # time.sleep(1)
         #data = conn.recv(1024)
         #print repr(data)
         #time.sleep(0.5)
-
+        print "Enter command track, redetect, stop"
+        command_state = 0
+        while 1:
+            input = raw_input()
+            if input.startswith('track'):
+                command_state = 1
+                break
+            elif input.startswith('redetect'):
+                command_state = 2
+                break
+            elif input.startswith('stop'):
+                command_state = 3
+                break
+            else:
+                print "rewrite command"
         #track
-        msg = {
-                'cmd': 'track',
-                'target' : {
-                    'object': 'car',
-                    'index': 0,
-                    }
-              }
-
+        if(command_state == 1):
+            msg = {
+                    'cmd': 'track',
+                    'target' : {
+                        'object': 'car',
+                        'index': 0,
+                        }
+                  }
         #redetect
-        msg = {
-                'cmd': 'redetect'
-                }
+        elif(command_state == 2):
+            msg = {
+                    'cmd': 'redetect'
+                    }
 
         #stop
-        msg = {
-                'cmd': 'stop'
-                }
-
-
+        elif(command_state == 3):
+            msg = {
+                    'cmd': 'stop'
+                    }
 
         output = json.dumps(msg)
         print "Send ",
